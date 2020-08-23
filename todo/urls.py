7dae96fc187750093b15d8c4d28463d4b3cf00ab
kushinoyuya <kusshy15059855@gmail.com>
2020-08-23 11:29:46 +0900

    20200823

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,9 @@
 
 from django.urls import path
-from .views import TodoList
+from .views import TodoList, TodoDetail
+
 
 urlpatterns = [
     path('list/', TodoList.as_view()),
+    path('detail/<int:pk>', TodoDetail.as_view()),
 ]
